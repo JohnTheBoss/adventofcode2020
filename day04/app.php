4e686ae52c3d4bc32f3fa43e3b1146fec775ec3f
JohnTheBoss <git@janoskollar.hu>
2020-12-04 22:23:51 +0100

    finish day04  ⭐⭐

--- day04/app.php
+++ day04/app.php
@@ -1,7 +1,15 @@
 <?php
 $handle = fopen("input.txt", "r");
 
-$requedPassportFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+$requedPassportFields = [
+    "byr" => "/^((19[2-9][0-9])|(200[0-2]))$/i",
+    "iyr" => "/^(20(1[0-9]|20))$/i",
+    "eyr" => "/^(20(2[0-9]|30))$/i",
+    "hgt" => "/^(((1[5-8][0-9]|19[0-3])cm)|(59|(6[0-9])|(7[0-6]))in)$/i",
+    "hcl" => "/^#[a-f,0-9]{6}$/i",
+    "ecl" => "/^(amb|blu|brn|gry|grn|hzl|oth)$/i",
+    "pid" => "/^[0-9]{9}$/i",
+];
 
 function parsePassportFields($line){
     $passportLine = explode(" ", $line);
@@ -11,18 +19,26 @@ function parsePassportFields($line){
         $parseKeyValue = explode(":", $data);
         $passportKey = $parseKeyValue[0];
         $passportValue = $parseKeyValue[1];
-        $parsedPassportData[$passportKey] = $passportValue;
+        $parsedPassportData[$passportKey] = trim(preg_replace('/\s\s+/', ' ', $passportValue));
     }
     return $parsedPassportData;
 }
 
-function passportIsValid($passport){
+function passportIsValid($passport, $echo = false){
     global $requedPassportFields;
     $validFieldNum = 0;
-    foreach ($requedPassportFields as $requedFieldName) {
-        if(array_key_exists($requedFieldName, $passport)){
-            $validFieldNum++;
+    foreach ($requedPassportFields as $fieldKey => $fieldRule) {
+        if(array_key_exists($fieldKey, $passport)){
+            if($echo){
+                echo $fieldKey . ":\t". $passport[$fieldKey] ."=>\t\t". preg_match($fieldRule, $passport[$fieldKey])."\n";
+            }
+            if(preg_match($fieldRule, $passport[$fieldKey])){
+                $validFieldNum++;
+            }
         }
+    }
+    if($echo){
+        echo "\n";
     }
     return $validFieldNum == count($requedPassportFields);
 }
@@ -42,10 +58,10 @@ if ($handle) {
 }
 
 $validPassport = 0;
-foreach($passengers as $passenger){
-    if(passportIsValid($passenger)){
+foreach($passengers as $index => $passenger){
+    if(passportIsValid($passenger, $index < 11)){
         $validPassport++;
     }
 }
 
-echo "day4a valid passport is $validPassport\n";
+echo "day4b valid passport is $validPassport\n";
