b9fa68e3d763a6e0a23bbea0bb90d59c318d582a
JohnTheBoss <git@janoskollar.hu>
2020-12-12 16:57:19 +0100

    ⭐⭐

--- day08/app.php
+++ day08/app.php
@@ -44,12 +44,11 @@ function nextStep($currentStep, $accumulator, $operate, $agv)
 function runProgram($program)
 {
     $accumulator = 0;
-    $accumulator8a = 0;
     $commandIndex = 0;
-
     $lineHistory = [];
+    $loopError = false;
 
-    while (!in_array($commandIndex, $lineHistory)) {
+    while (!in_array($commandIndex, $lineHistory) && $commandIndex < count($program)) {
         $lineHistory[] = $commandIndex;
         $run = nextStep($commandIndex, $accumulator, $program[$commandIndex]['operate'], $program[$commandIndex]['arg']);
         $commandIndex = $run['next'];
@@ -57,8 +56,38 @@ function runProgram($program)
         // echo $commandIndex . ".\t" . $program[$commandIndex]['operate'] . " " . $program[$commandIndex]['arg'] . "\t" . $accumulator . "\n";
     }
 
-    $accumulator8a = $accumulator;
-    echo "8a acc is: " . $accumulator8a . "\n";
+    if (in_array($commandIndex, $lineHistory) || $commandIndex > count($program)) {
+        $loopError = true;
+    }
+
+    return [$loopError, $accumulator];
 }
 
-runProgram($program);
+echo "8a acc is: " . runProgram($program)[1] . "\n";
+
+function fixProgram($program)
+{
+    $testLine = 0;
+    $accumulator = 0;
+
+    $loopError = true;
+    while ($loopError) {
+        $testProgram = $program;
+
+        if ($testProgram[$testLine]['operate'] == 'jmp') {
+            $testProgram[$testLine]['operate'] = "nop";
+        } else if ($testProgram[$testLine]['operate'] == 'nop') {
+            $testProgram[$testLine]['operate'] = 'jmp';
+        }
+
+        $run = runProgram($testProgram);
+        $loopError = $run[0];
+        $accumulator = $run[1];
+
+        $testLine++;
+    }
+
+    return $accumulator;
+}
+
+echo "8b acc is: " . fixProgram($program) . "\n";
